1d6c307ba2b3cde9fd86f64dcacd44709f845a4d
Kamran Rabiei <kamranrabiei96@gmail.com>
2025-06-08 00:57:11 +0330

    no

--- src/realm/schemas/stage_game/StageSeasonSchema.ts
+++ src/realm/schemas/stage_game/StageSeasonSchema.ts
@@ -5,37 +5,30 @@ export class StageSeason extends Realm.Object<StageSeason> {
   title!: string;
   description?: string;
   language?: BSON.ObjectId;
-
   media!: {
     path: string;
     order?: number;
     file_type?: string;
     duration?: string;
   }[];
-
   music?: {
     path?: string;
     file_type?: string;
     duration?: string;
   };
-
   badg?: string;
   season_number?: number;
   stage_number_from?: number;
   stage_number_to?: number;
   number_stage?: number;
-
   is_visible: boolean = false;
   is_active: boolean = false;
-
   content_source_type!: "original" | "derived" | "copy";
   publication_status!: "draft" | "ready" | "published" | "archived" | "rejected";
   completion_status!: "incomplete" | "in_progress" | "complete" | "finalized";
-
   version_created?: number;
   version_updated?: number;
   version_deleted?: number;
-
   createdAt!: Date;
   updatedAt!: Date;
 
@@ -47,31 +40,24 @@ export class StageSeason extends Realm.Object<StageSeason> {
       title: "string",
       description: "string?",
       language: "objectId?",
-
       media: {
         type: "list",
         objectType: "StageSeasonMedia",
       },
-
       music: "StageSeasonMusic?",
-
       badg: "string?",
       season_number: "int?",
       stage_number_from: "int?",
       stage_number_to: "int?",
       number_stage: "int?",
-
       is_visible: { type: "bool", default: false },
       is_active: { type: "bool", default: false },
-
       content_source_type: "string?",
       publication_status: "string?",
       completion_status: "string?",
-
       version_created: "int?",
       version_updated: "int?",
       version_deleted: "int?",
-
       createdAt: "date",
       updatedAt: "date",
     },
