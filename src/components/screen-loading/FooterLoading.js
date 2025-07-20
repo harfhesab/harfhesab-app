dca0f3bcbeba41b743caebf79d747e8add778026
Kamran Rabiei <kamranrabiei96@gmail.com>
2025-07-20 19:06:33 +0330

    some

---
+++ src/components/screen-loading/FooterLoading.js
@@ -0,0 +1,31 @@
+import React, {memo} from 'react';
+import {StyleSheet, View, Dimensions, TouchableOpacity, Text, ActivityIndicator} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import Font from '../../utils/Font';
+
+const width = Dimensions.get('window').width
+function FooterLoading({loading, tryAgain, tryOperation}){
+    const {colors} = useTheme().colors;
+    return(
+        tryAgain == true?
+        (<View style={styles.container}>
+            <TouchableOpacity activeOpacity={0.7} onPress={tryOperation} style={{alignItems:'center', justifyContent:'center', borderRadius:5, borderWidth:1, borderColor:colors.border.a1, width:(width-30)/2, height:35}}>
+                <Text style={{color:colors.text.a2, fontSize:12, fontFamily:Font.medium}}>{'تلاش دوباره'}</Text>
+            </TouchableOpacity>
+        </View>)
+        :loading == true?
+        (<View style={styles.container}>
+            <ActivityIndicator color={colors.color} size={'large'}/>
+        </View>)
+        :null
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        height:60,
+        width:width,
+        alignItems: 'center',
+        justifyContent: "center"
+    },
+})
+export default memo(FooterLoading);
--- src/screens/main/package-game/PackageGame.js
+++ src/screens/main/package-game/PackageGame.js
@@ -1,35 +1,245 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text, Dimensions, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect, useMemo} from 'react';
+import {StyleSheet, Platform, View, Text, Dimensions, TouchableOpacity} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 import LinearGradient from 'react-native-linear-gradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import FooterLoading from '../../../components/screen-loading/FooterLoading';
+import ScreenLoading from '../../../components/screen-loading/ScreenLoading';
 
 const {width, height} = Dimensions.get("window")
 function PackageGame(props){
     const {colors} = useTheme().colors;
+    const [banner, setBanner] = useState([])
+    const [collection, setCollection] = useState([])
+    const [page, setPage] = useState(1)
     const [loading, setLoading] = useState(true)
+    const [refreshing, setRefreshing] = useState(false)
     const [getError, setGetError] = useState(false)
+    const [noItem, setNoItem] = useState(false)
+    const [footerTry, setFooterTry] = useState(false)
+    const [footerLoading, setFooterLoading] = useState(false)
 
     useEffect(()=>{
-        getData()
+        getDataForFirst()
     }, [])
-    const getData = async()=>{
-        
+    const getDataForFirst = async()=>{
+        await axios({
+            url:'/',
+            method:'post',
+            data: {
+                query : `
+                    query paginatePackageGameCollectionAndOther(
+                        $page : Int,
+                        $limit : Int,
+                    ){
+                        paginatePackageGameCollectionAndOther(
+                            page : $page,
+                            limit : $limit,
+                        ) {
+                            banner{
+                                path,
+                                file_type,
+                                duration,
+                                click_type,
+                                link,
+                                navigate,
+                                params_id,
+                                params_other,
+                                order,
+                                page,
+                                is_visible,
+                                is_active,
+                            },
+                            collection{
+                                _id,
+                                title,
+                                list{_id, title, icon_image},
+                                is_visible,
+                                is_active,
+                            },
+                            hasNextPage,
+                            nextPage
+                        }
+                    }
+                `,
+                variables : {
+                    "page" : 1,
+                    "limit" : 10,
+                }
+            }
+        }).then(async(response)=>{
+            const dataReceived = response.data.data?.paginatePackageGameCollectionAndOther
+            if(dataReceived.hasNextPage == true){
+                setLoading(false)
+                setCollection(dataReceived.collection)
+                setPage(dataReceived.nextPage)
+                setFooterLoading(true)
+            } else {
+                if(dataReceived.collection.length > 0){
+                    setFooterLoading(false)
+                    setLoading(false)
+                    setCollection(dataReceived.collection)
+                    setPage(1)
+                } else {
+                    setFooterLoading(false)
+                    setLoading(true)
+                    setCollection([])
+                    setNoItem(true)
+                }
+            }
+            setRefreshing(false)
+        }).catch(()=>{
+            setFooterLoading(false)
+            setLoading(true)
+            setFooterTry(false)
+            setGetError(true)
+            setCollection([])
+            setRefreshing(false)
+        })
     }
-    const tryAgain = ()=>{
+    const getDataForMore = async()=>{
+        await axios({
+            url:'/',
+            method:'post',
+            data: {
+                query : `
+                    query paginatePackageGameCollectionAndOther(
+                        $page : Int,
+                        $limit : Int,
+                    ){
+                        paginatePackageGameCollectionAndOther(
+                            page : $page,
+                            limit : $limit,
+                        ) {
+                            banner{
+                                path,
+                                file_type,
+                                duration,
+                                click_type,
+                                link,
+                                navigate,
+                                params_id,
+                                params_other,
+                                order,
+                                page,
+                                is_visible,
+                                is_active,
+                            },
+                            collection{
+                                _id,
+                                title,
+                                list{_id, title, icon_image},
+                                is_visible,
+                                is_active,
+                            },
+                            hasNextPage,
+                            nextPage
+                        }
+                    }
+                `,
+                variables : {
+                    "page" : page,
+                    "limit" : 10,
+                }
+            }
+        }).then(async(response)=>{
+            const dataReceived = response.data.data?.paginatePackageGameCollectionAndOther
+            if(dataReceived.hasNextPage == true){
+                setCollection([...collection, ...dataReceived.collection])
+                setPage(dataReceived.nextPage)
+                setFooterLoading(true)
+            } else {
+                if(dataReceived.collection.length > 0){
+                    setFooterLoading(false)
+                    setCollection([...collection, ...dataReceived.collection])
+                    setPage(page)
+                } else {
+                    setFooterLoading(false)
+                    setNoItem(collection.length > 0?false:true)
+                    setLoading(collection.length > 0?false:true)
+                }
+            }
+        }).catch(()=>{
+            setLoading(collection.length > 0?false:true)
+            setFooterTry(collection.length > 0?true:false)
+            setGetError(collection.length > 0?false:true)
+        })
+    }
+    const tryAgain = async()=>{
+        setFooterLoading(false)
         setLoading(true)
         setGetError(false)
-        getData()
+        setNoItem(false)
+        setPage(1)
+        setFooterTry(false)
+        getDataForFirst()
     }
+    const footertryAgain = async()=>{
+        setFooterLoading(true)
+        setFooterTry(false)
+        getDataForMore()
+    }
+    const fetchMoreData = ()=>{
+        if(footerLoading == true && loading == false){
+            getDataForMore()
+        }
+    }
+    const onRefresh = ()=>{
+        setRefreshing(true)
+        getDataForFirst()
+    }
+    const renderFooter = ()=>{
+        if(loading == false && collection.length > 0){
+            return(
+                <FooterLoading
+                    loading={footerLoading}
+                    tryAgain={footerTry}
+                    tryOperation={footertryAgain}
+                />
+            )
+        }
+    }
+    const renderItem = useCallback(({item})=>(
+        <View>
+
+        </View>
+    ), [])
+    const memoizedValue = useMemo(() => renderItem, [data]);
+    const keyExtractor = (item,index)=>index.toString()
     
     return(
-        <SafeAreaView>
+         <View style={[styles.container, {backgroundColor:colors.background}]}> 
+            <GeneralHeader
+                back={false}
+                title={"بروزرسانی محتوای بازی مرحله‌ای"}
+            />
             <LinearGradient colors={colors.background_gradient} style={{width:width, height:height}}>
                 <View style={styles.container}>
-
+                    {
+                        loading == true?
+                        <ScreenLoading
+                            loading={loading}
+                            getError={getError}
+                            noItem={noItem}
+                            tryAgain={tryAgain}
+                        />
+                        :
+                        <FlatList
+                            style={{flex:1}}
+                            showsVerticalScrollIndicator={true}
+                            data={collection}
+                            keyExtractor={keyExtractor}
+                            renderItem={memoizedValue}
+                            ListFooterComponent={renderFooter}
+                            onEndReached={fetchMoreData}
+                            onEndReachedThreshold={0.5}
+                            initialNumToRender={20}
+                            removeClippedSubviews={Platform.OS == 'ios' ? false : true}
+                        />
+                    } 
                 </View>
-            </LinearGradient>
-        </SafeAreaView>
+            </LinearGradient> 
+        </View>
     )
 }
 const styles = StyleSheet.create({
